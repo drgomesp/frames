6feab2f22bd26baa5bad646551b34cdac25bb32b
Daniel Ribeiro <drgomesp@gmail.com>
2019-06-13 20:20:47 -0300

    Added most basic example of a request to TMDb API

--- movies-api/go.mod
+++ movies-api/go.mod
@@ -3,6 +3,7 @@ module github.com/drgomesp/frames/movies-api
 go 1.12
 
 require (
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.2.9 // indirect
 	golang.org/x/crypto v0.0.0-20190611184440-5c40567a22f8 // indirect
--- movies-api/main.go
+++ movies-api/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"fmt"
 	"net/http"
 	
 	"github.com/labstack/echo"
@@ -8,8 +11,63 @@ import (
 
 func main() {
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
+		fetchUpcomingMovies()
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+type UpcomingMoviesResponse struct {
+	Results []MovieListResult `json:"results"`
+	TotalPages int `json:"total_pages"`
+	TotalResults int `json:"total_results"`
+}
+
+type MovieListResult struct {
+	PosterPath string `json:"poster_path"`
+	Adult bool `json:"adult"`
+	Overview string `json:"overview"`
+	ReleaseDate string `json:"release_date"`
+	GenreIDs []int `json:"genre_ids"`
+	ID int `json:"id"`
+	OriginalTitle string `json:"original_title"`
+	OriginalLanguage string `json:"original_language"`
+	Title string `json:"title"`
+	BackdropPath string `json:"backdrop_path"`
+	Popularity float32 `json:"popularity"`
+	VoteCount int `json:"vote_count"`
+	Video bool `json:"video"`
+	VoteAverage float32 `json:"vote_average"`
+}
+
+func fetchUpcomingMovies() {
+	url := fmt.Sprintf("https://api.themoviedb.org/3/movie/upcoming?api_key=1f54bd990f1cdfb230adb312546d765d&language=en-US&page=1")
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var r UpcomingMoviesResponse
+
+	// Use json.Decode for reading streams of JSON data
+	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+		log.Println(err)
+	}
+
+	log.Println(r.TotalPages)
+	log.Println(r.TotalResults)
 }
